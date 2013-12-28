fc419775b26ffeb1bd5c10c2cdb3f1a0c78958a6
Axel Haustant <noirbizarre@gmail.com>
2013-12-28 15:41:14 +0100

    Use json for translations

--- Etalab.i18n.php
+++ Etalab.i18n.php
@@ -8,60 +8,13 @@
 
 $messages = array();
 
-/**
- * English
- * @author Axel Haustant
+/*
+ * Load translations from json files
  */
-$messages['en'] = array(
-        'skinname-etalab' => "ETALAB",
-        'etalab-desc' => "Bootstrap skin for Etalab.",
-        'etalab-site-desc' => 'Open platform for french public data',
-        'back-to-top' => 'Back to top',
-        'sign-in-register' => 'Sign In / Register',
-        'profile' => 'Profile',
-        'privacy' => 'Privacy',
-        'terms' => 'Terms',
-        'where' => 'Where ?',
-        'publish-dataset' => 'Publish a dataset !',
-        'publish-short' => 'Publish !',
-        'faq' => 'How it works ?',
-        'publishers' => 'Publishers',
-        'open-licence' => 'Open Licence',
-        'topics' => 'Topics',
-        'network' => 'Network',
-        'contact' => 'Contact',
-        'credits' => 'Credits',
-        'metrics' => 'Metrics',
-        'open-data' => 'The Open Data',
-        'home' => 'Home',
-        'terms-of-use' => 'Terms of use',
-);
-
-/**
- * French
- * @author Axel Haustant
- */
-$messages['fr'] = array(
-        'skinname-etalab' => "ETALAB",
-        'etalab-desc' => "Skin bootstrap pour Etalab.",
-        'etalab-site-desc' => 'Plateforme ouverte des données publiques françaises',
-        'back-to-top' => 'Haut de page',
-        'sign-in-register' => 'Se connecter / S\'inscrire',
-        'profile' => 'Profil',
-        'privacy' => 'Vie privée',
-        'terms' => 'Conditions légales',
-        'where' => 'Où ?',
-        'publish-dataset' => 'Publiez une série de données !',
-        'publish-short' => 'Publiez !',
-        'faq' => 'Comment ça marche ?',
-        'publishers' => 'Producteurs',
-        'open-licence' => 'Licence Ouverte',
-        'topics' => 'Thématiques',
-        'network' => 'Réseau',
-        'contact' => 'Contact',
-        'credits' => 'Credits',
-        'metrics' => 'Métriques',
-        'open-data' => "L'Open Data",
-        'home' => 'Accueil',
-        'terms-of-use' => "Conditions générales d'utilisation",
-);
+$files = glob(dirname(__FILE__).'/i18n/*.json');
+foreach( $files as $file ) {
+    $lang = substr( basename( $file ), 0, -5 );
+    $data = (array)json_decode( file_get_contents( $file ) );
+    unset( $data['@metadata'] );
+    $messages[$lang] = $data ;
+}
